aa721f873b099981b9430a009790c2a91ebb3f1c
Vladimir <andiwillfly@gmail.com>
2021-02-03 12:34:42 +0200

    dd

--- src/components/Map.cmponent.js
+++ src/components/Map.cmponent.js
@@ -1,16 +1,8 @@
 import React from "react"
-import mapboxgl from 'mapbox-gl';
-import L from 'leaflet';
 import 'leaflet.markercluster';
-import LG from "leaflet-geometryutil";
-import DB from "../server/DB.json";
 import "../map/canvasIcon";
-import canvasMarker from "../map/canvasMarker";
 // Models
 import mapModel from "../models/map.model";
-
-
-const TOKEN = 'pk.eyJ1IjoiYW5kaXdpbGxmbHkiLCJhIjoiY2s2cW1qajhoMHB3MDNzcW81dmM4bDlkMSJ9.gmA_WZGL_NxHa4hdx9sttA'
 
 class Map extends React.Component {
 
@@ -19,53 +11,14 @@ class Map extends React.Component {
 
 
     componentDidMount() {
-        mapboxgl.accessToken = TOKEN;
-        mapModel.setup();
-
-        const myIcon = L.canvasIcon({drawIcon: canvasMarker});
-
-        //const markers = L.markerClusterGroup();
-
-        let markers = [];
-        DB.offers.forEach(offer => {
-            const marker = L.marker([
-                offer.geo.displayPosition.latitude,
-                offer.geo.displayPosition.longitude
-            ], {icon: myIcon, ...offer})
-                .addTo(mapModel.map)
-                .on('click', (e) => {
-                    console.log('clisds!');
-                    const offer = e.sourceTarget.options;
-                    mapModel.drawPopup(L, e.latlng, offer);
-                });
-            markers.push(marker);
-           // markers.addLayer(marker);
-        });
-
-        //mapModel.map.addLayer(markers);
-
-        // markers.on('click', function (e) {
-        //     console.log('marker ', e);
-        //     const offer = e.sourceTarget.options;
-        //     mapModel.drawPopup(L, e.latlng, offer);
-        // });
-
-        mapModel.map.on('click', function (e) {
-            console.log('3', markers, L.GeometryUtil.closest(mapModel.map, markers, e.latlng));
-
-            // for (var i = 0; i < markers.length; i++) {
-            //     if (e.boxZoomBounds.contains(markers[i])) {
-            //         console.log(markers[i]);
-            //     }
-            // }
-        });
+        mapModel.setup2();
     }
 
 
     render() {
         return (
             <>
-                <div id='map' style={{ width: '100vw', height: '90vh' }} />
+                <div id='map' style={{ width: '100vw', height: '100vh' }} />
             </>
         );
     }
--- src/models/map.model.js
+++ src/models/map.model.js
@@ -1,6 +1,19 @@
 // MobX
 import { makeAutoObservable } from 'mobx';
+import DB from "../server/DB.json";
 // Helpers
+// Map Ol
+import 'ol/ol.css';
+import Feature from 'ol/Feature';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import Point from 'ol/geom/Point';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { Circle as CircleStyle, Fill, Stroke, Icon, Style } from 'ol/style';
+import { fromLonLat } from 'ol/proj';
+import VectorSource from 'ol/source/Vector';
+import OSM from 'ol/source/OSM';
+// Other
 import L from "leaflet";
 import * as ComLink from "comlink";
 
@@ -27,6 +40,73 @@ class MapModel {
         console.log(`Counter: ${await obj.counter}`);
     }
 
+
+    setup2() {
+        const vectorSource = new VectorSource({
+            features: DB.offers.map(offer => {
+                const flatDot = new Feature({
+                    ...offer,
+                    geometry: new Point(fromLonLat([offer.geo.displayPosition.longitude, offer.geo.displayPosition.latitude]))
+                });
+
+                flatDot.setStyle(new Style({
+                    image: new CircleStyle({
+                        radius: 4,
+                        fill: new Fill({
+                            color: 'blue'
+                        }),
+                        stroke: new Stroke({ color: 'white', width: 1 })
+                    })
+                }));
+
+                return flatDot;
+            })
+        });
+
+        this.vectorLayer = new VectorLayer({
+            source: vectorSource
+        });
+
+        const tileLayer = new TileLayer({
+            source: new OSM()
+        });
+
+        this.map = new Map({
+            layers: [tileLayer, this.vectorLayer],
+            target: document.getElementById('map'),
+            view: new View({
+                center: fromLonLat([30.5234, 50.4501]),
+                zoom: 11
+            })
+        });
+
+        this.map.set('layers', []);
+        this.map.render();
+
+        this.map.on('click', (event)=> {
+            const features = this.map.getFeaturesAtPixel(event.pixel);
+
+            console.log(features, 2);
+            //this.hoveredFlatsData.ids = [];
+            // features.forEach(feature => {
+            //     this.flats[feature.values_.link].flat.isVisited = true;
+            // });
+            // this.onSearch({ target: { value: document.querySelector('input').value }});
+            //this.clickedFlats.ids = features.map(feature => feature.values_.link);
+        });
+
+        this.map.on('pointermove', (event)=> {
+            //if(event.dragging) return this.hoveredFlatsData.ids = [];
+
+            const pixel = this.map.getEventPixel(event.originalEvent);
+            const features = this.map.getFeaturesAtPixel(pixel);
+
+            // this.hoveredFlatsData.ids = features.map(feature => feature.values_.link);
+            // this.hoveredFlatsData.left = pixel[0];
+            // this.hoveredFlatsData.top = pixel[1] + 25;
+            this.map.getViewport().style.cursor = features.length ? 'pointer' : 'inherit';
+        });
+    }
 
     setup() {
         this.map =  L.map('map').setView([50.4501, 30.5234], 12);
