f46e19c42875678b8521675bb20cd1dd45b1e678
Vladimir <andiwillfly@gmail.com>
2021-02-12 15:42:42 +0200

    d

---
+++ src/server/parser/parsers/100realty.ua.parser.js
@@ -0,0 +1,70 @@
+const cliProgress = require('cli-progress');
+let progress = null;
+
+
+module.exports = async function(browser, { url, info }) {
+
+    console.log(`✨ ${info} PARSER:START`);
+    try {
+        const offers = await parsePage(browser, url, 0, [], info);
+        progress.stop();
+        return offers;
+    } catch(e) {
+        console.log(`✨ ${info} PARSER: PAGE ERROR | `, e);
+        progress.stop();
+        return [];
+    }
+
+}
+
+async function parsePage(browser, url, number = 0, offers = [], info) {
+    const page = await browser.newPage();
+    await page.goto(`${url}&page=${number}`, {
+        // waitUntil: 'load',
+        // timeout: 0
+    });
+
+    // Small site (~1-2 pages) - no need to parse more than 4
+    const isLastPage = number > 3 || await page.evaluate(()=> {
+        return ![...document.querySelectorAll('.listingv2 .listgrid')].length;
+    });
+
+    if(!progress) {
+        progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+        progress.start(4, 0);
+    } else {
+        progress.increment();
+    }
+
+    if(isLastPage) return offers;
+
+    const rows = await page.evaluate((_info)=> {
+        return [...document.querySelectorAll('.listingv2 .listgrid')].map($item => {
+            const $img = $item.querySelector('.owl-item.active img');
+            const $floor = $item.querySelector('.listingv2-paramblock-left .listingv2-param-floor');
+            const $square = $item.querySelector('.listingv2-paramblock-left .listingv2-param-areas');
+            const link = $item.querySelector('a').getAttribute('href');
+            const isNotReady = $item.querySelector('.listingv2-paramblock-right .listingv2-param.listingv2-ellipsed').innerText.includes('Термін виплат');
+
+            if(isNotReady) return null;
+            return {
+                id: link.startsWith('http') ? link : `https://bn.ua${link}`,
+                img: $img ? `https://bn.ua${$img.getAttribute('src')}` : '',
+                title: $item.querySelector('.listingv2-param-addr').innerText,
+                link: link.startsWith('http') ? link : `https://bn.ua${link}`,
+                address: `Киев, ${$item.querySelector('.listingv2-param-addr').innerText}`,
+                price: +$item.querySelector('.val').innerText.replace(/ /g, '').match(/\d+/g)[0],
+                floor: $floor ? $floor.innerText.match(/\d+/g).map(f => +f) : 0,
+                rooms: +$item.querySelector('.listingv2-paramblock-left .listingv2-param-rooms').innerText.match(/\d+/g)[0],
+                square: $square ? +$square.innerText.match(/\d+/)[0] : 0,
+
+                source: _info
+            }
+        }).filter(Boolean);
+    }, info);
+
+    rows.forEach(row => offers.push(row));
+
+    await page.close();
+    return await parsePage(browser, url,number+1, offers, info);
+}
---
+++ src/server/parser/parsers/rieltor.ua.parser.js
@@ -0,0 +1,71 @@
+const cliProgress = require('cli-progress');
+let progress = null;
+
+
+module.exports = async function(browser, { url, info }) {
+
+    console.log(`✨ ${info} PARSER:START`);
+    try {
+        const offers = await parsePage(browser, url, 1, [], info);
+        progress.stop();
+        console.log(`✨ ${info} PARSER:END`);
+        return offers;
+    } catch(e) {
+        console.log(`✨ ${info} PARSER: PAGE ERROR | `, e);
+        progress.stop();
+        return [];
+    }
+
+}
+
+
+let totalPages = 0;
+async function parsePage(browser, url, number = 0, offers = [], info) {
+    const page = await browser.newPage();
+    await page.goto(`${url}?page=${number}`, {
+        // waitUntil: 'load',
+        // timeout: 0
+    });
+
+    totalPages = totalPages || await page.evaluate(()=> {
+        const $links = document.querySelectorAll('.pager-item');
+        if(!$links) return totalPages;
+        return +$links[$links.length-2].innerText
+    });
+
+    if(!progress) {
+        progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+        progress.start(totalPages, 0);
+    } else {
+        progress.increment();
+    }
+
+    if(totalPages <= number) return offers;
+
+    const rows = await page.evaluate((_info)=> {
+        return [...document.querySelectorAll('.realty-object-card') ].map($row => {
+            const link = $row.querySelector('.object-address > a') && $row.querySelector('.object-address > a').getAttribute('href');
+            if(!link) return;
+            return {
+                id: $row.querySelector('.object-address > a').innerText,
+                img: $row.querySelector('.slides__item img') ? $row.querySelector('.slides__item img').getAttribute('src') : 'https://www.samsung.com/etc/designs/smg/global/imgs/support/cont/NO_IMG_600x600.png',
+                title: $row.querySelector('.object-address > a').innerText,
+                link: `https://100realty.ua${link}`,
+                address: $row.querySelector('.object-address > a').innerText,
+
+                price: parseInt($row.querySelector('.usd-price-value').innerText.replace(/ /g, '').match(/\d+/g)[0]),
+                floor: 0,
+                rooms: parseInt($row.querySelector('.object-rooms .value').innerText.replace(/ /g, '').match(/\d+/g)[0]),
+                square: parseInt($row.querySelector('.object-square .value').innerText.replace(/ /g, '').match(/\d+/g)[0]),
+                description: $row.querySelector('.descr').innerText,
+
+                source: _info
+            }
+        }).filter(Boolean);
+    }, info);
+
+    rows.forEach(row => offers.push(row));
+
+    await page.close();
+    return await parsePage(browser, url, number+1, offers, info);
+}
